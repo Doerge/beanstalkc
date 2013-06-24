28aeb2ccf4bea2d5db5ad406911e087e17632ddc
Aske Dørge <aske.doerge@gmail.com>
2013-06-24 14:14:06 +0200

    Initial asynch-reconnect. Also added comments for some methods.

--- beanstalkc.py
+++ beanstalkc.py
@@ -23,7 +23,7 @@ import logging
 import socket
 import random
 import time
-
+import threading
 
 DEFAULT_HOST = 'localhost'
 DEFAULT_PORT = 11300
@@ -46,33 +46,68 @@ class SocketError(BeanstalkcException):
 
 class Pool(object):
     """ Pool class enclosing multiple Connection-objects. The constructor
-    expects a list of (str(host),int(port)) as input."""
+    expects a list of tuples, (str(host),int(port)) as input."""
     def __init__(self,bstalks):
+        self.conLock = threading.RLock()
         self.bstalks = bstalks
         self.connections = []
-        self.connect()
-        
-        if (len(self.connections) == 0):
-          raise BeanstalkcException('No connections could be reached.')
-        
-        # Gather the sockets in a dict as tuples of (socket,connection) so we
-        # can resolve socket objects to their connections.
-        self._sockets = dict()
-        for conn in self.connections:
-            self._sockets[conn._socket] = conn
+        self.init_connections()
+    
+    def init_connections(self):
+        threads = []
+        for (host,port) in self.bstalks:
+            t = threading.Thread(target=self._connect, args=(host,int(port)))
+            t.start()
+    
+    def _connect(self,host,port):
+        """Attempt connecting with beanstalkd at host:port. If successful add
+        to the connections-list and return. Else sleep, and try again."""
+        sleep_time = 2
+        while (True):
+            try:
+                conn = Connection(host=host, port=port, parse_yaml=True,
+                                  connect_timeout=5)
+                with self.conLock:
+                    self.connections.append( conn )
+                return
+            except SocketError, e:
+                logging.error('beanstalkc-pool failed connecting to %s %d. Retrying in a while.' % (host,port))
+                time.sleep(sleep_time)
+                sleep_time += sleep_time % 64
     
     def _call_wrap(self,conn,func,args):
-        if (args == None):
-            return func(conn)
-        else:
-            return func(conn, args )
+        """Handles the call of Connection-functions. Also responsible for
+        spawning threads to reconnect with lost Connections."""
+        try:
+            if (args == None):
+                return func(conn)
+            else:
+                return func(conn, args )
+        except SocketError as e:
+            with self.conLock:
+                self.connections.remove(conn)
+            t = threading.Thread(target=self._connect, args=(conn.host,conn.port))
+            t.start()
+            logging.error('beanstalkc-pool socket-error to %s %d. Started reconnect-thread.' % (conn.host,conn.port))
+            raise e
     
     def _send_to_rand_conn(self,func,args=None):
-        """Send to a random connection in the pool. Call like this:
-        self._send_to_rand_conn(some_func, ) if there are no arguments to the
-        function. Returns a tuple of the connection the command was sent to,
-        and its response."""
-        conn = random.choice(self.connections)
+        """Send to a random connection in the pool. Returns a tuple of the
+        connection the command was sent to, and its response."""
+        retries = 5
+        while (True):
+            try:
+                with self.conLock:
+                    conn = random.choice(self.connections)
+                break
+            except IndexError as e:
+                # self.connections is empty.. Sleep for a little while and try
+                # again.
+                if (not retries):
+                    raise SocketError('No connections available.')
+                time.sleep(2)
+            retries -= 1
+        
         resp = self._call_wrap(conn,func,args)
         return (conn,resp)
     
@@ -80,12 +115,18 @@ class Pool(object):
         """Send to all connections in the pool. Returns a list of tuples
         (conn,response)"""
         results = []
-        for conn in self.connections:
-            results.append( (conn,self._call_wrap(conn,func,args)) )
+        with self.conLock:
+            # Loop over a copy of self.connections (slice notation) to
+            # encompass changes to itself, during the loop.
+            for conn in self.connections[:]:
+                try:
+                    results.append( (conn,self._call_wrap(conn,func,args)) )
+                except SocketError as e:
+                    logging.error('beanstalkc-pool connection error in _send_to_all(). Skipping connection.')
         return results
     
     def reserve(self,pool_timeout=None,timeout=10):
-        """Reserves a job from the pool.
+        """Reserves a job from the pool randomly.
         pool_timeout: Overall time in seconds before this call returns.
              timeout: Individual reserve-calls timeouts.
         
@@ -94,28 +135,27 @@ class Pool(object):
         if (pool_timeout != None):
             timer = time.time()
         while (True):
-            conn = random.choice(self.connections)
             try:
+                with self.conLock:
+                    conn = random.choice(self.connections)
                 result = self._call_wrap(conn, Connection.reserve, timeout)
-                # If result == None, the reserve timed-out.
+                # If result == None, the reserve timed-out and we try again.
                 if (result != None):
                     return (conn,result)
             except DeadlineSoon as e:
+                # Deadlinesoon. We give up and try reserving another job.
                 pass
+            except SocketError as e:
+                # The connection we chose failed. Trying another.. (it has been
+                # removed from the connections list..)
+                pass
+            except IndexError as e:
+                # self.connections is empty.. Sleep for a little while and try again.
+                time.sleep(2)
             if (timer != None and time.time() > timer + pool_timeout):
-                break
-        # If we get here, the pool-timeout has been reached. Returning None
-        # just as Connection.reserve when the timeout is reached.
-        return (None,None)
-    
-    def connect(self):
-        for (host,port) in self.bstalks:
-            try:
-                conn = Connection(host=host, port=int(port), parse_yaml=True,
-                                  connect_timeout=socket.getdefaulttimeout())
-                self.connections.append( conn )
-            except SocketError, e:
-                logging.error('beanstalkc-pool failed connection to %s %d' % (host,int(port)))
+                # If we get here, the pool-timeout has been reached. Returning
+                # None just as Connection.reserve when the timeout is reached.
+                return (None,None)
     
     def close(self):
         """Close all connections in the pool."""
@@ -214,7 +254,7 @@ class Connection(object):
     def _read_response(self):
         line = SocketError.wrap(self._socket_file.readline)
         if not line:
-            raise SocketError()
+            raise SocketError('SocketError')
         response = line.split()
         return response[0], response[1:]
 
